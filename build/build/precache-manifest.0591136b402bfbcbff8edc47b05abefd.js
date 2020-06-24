4c88c4605b3604080ceb7abc80ecb511866d3b88
Lokesh Bolisetty <63059375+LokeshBolisetty@users.noreply.github.com>
2020-06-24 12:58:55 +0530

    Changed .gitignore

---
+++ build/build/precache-manifest.0591136b402bfbcbff8edc47b05abefd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d83294d69ac4632ec73d0fe4fdb4ff4",
+    "url": "/LokeshBolisetty/authorwrites-admin/index.html"
+  },
+  {
+    "revision": "0dc9a5c16f471b9d8ea6",
+    "url": "/LokeshBolisetty/authorwrites-admin/static/css/main.cab58729.chunk.css"
+  },
+  {
+    "revision": "4dd343d2afa2aa48ea2e",
+    "url": "/LokeshBolisetty/authorwrites-admin/static/js/2.78c6c517.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/LokeshBolisetty/authorwrites-admin/static/js/2.78c6c517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc9a5c16f471b9d8ea6",
+    "url": "/LokeshBolisetty/authorwrites-admin/static/js/main.4b3c4577.chunk.js"
+  },
+  {
+    "revision": "299865f376f1ab1c6233",
+    "url": "/LokeshBolisetty/authorwrites-admin/static/js/runtime-main.b9628d05.js"
+  }
+]);
