621cf0425191f8c5674bce8d311488cab209a7eb
Lokesh Bolisetty <63059375+LokeshBolisetty@users.noreply.github.com>
2020-06-24 11:53:08 +0530

    Did npm run build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./App.css";
-import Welcome from "./Components/Welcome/index";
-import CardCounter from "./Containters/CardCounter";
 import Login from "./login";
 
 function App() {
