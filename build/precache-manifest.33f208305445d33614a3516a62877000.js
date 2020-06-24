bec4a1de50afc2b1fc3de4fe1ccb00891d47bb68
Lokesh Bolisetty <63059375+LokeshBolisetty@users.noreply.github.com>
2020-06-24 11:59:57 +0530

    Rechecked the files

---
+++ build/precache-manifest.33f208305445d33614a3516a62877000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5745e4e75784c02c775f8b677149d7",
+    "url": "/LokeshBolisetty/AutthorWrites-Admin/index.html"
+  },
+  {
+    "revision": "98a2f351accdcaa9908a",
+    "url": "/LokeshBolisetty/AutthorWrites-Admin/static/css/main.cab58729.chunk.css"
+  },
+  {
+    "revision": "4dd343d2afa2aa48ea2e",
+    "url": "/LokeshBolisetty/AutthorWrites-Admin/static/js/2.78c6c517.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/LokeshBolisetty/AutthorWrites-Admin/static/js/2.78c6c517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a2f351accdcaa9908a",
+    "url": "/LokeshBolisetty/AutthorWrites-Admin/static/js/main.d27e469a.chunk.js"
+  },
+  {
+    "revision": "4ee32f4a337d1727cc56",
+    "url": "/LokeshBolisetty/AutthorWrites-Admin/static/js/runtime-main.a76d74d8.js"
+  }
+]);
